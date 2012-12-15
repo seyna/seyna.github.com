8adc5fbde2f080c53019a208ac6f87bf0783cca8
seyna <closetu@gmail.com>
2012-12-15 00:10:35 -0800

    Pelican output more test

---
+++ source/posts/2012/12/more-test.md
@@ -0,0 +1,6 @@
+Date: 2012-12-15
+Title: More test
+Category: Blog
+Tags: blogging, test
+
+中文耶，好爽
--- source/settings.py
+++ source/settings.py
@@ -1,40 +1,51 @@
-"""Settings for pelican."""
-
-# This can also be the absolute path to a theme that you downloaded
-# i.e. './themes/anothertheme/'
-THEME = 'notmyidea'
-
-# The folder ``images`` should be copied into the folder ``static`` when
-# generating the output.
-STATIC_PATHS = ['images', ]
-
-# See http://pelican.notmyidea.org/en/latest/settings.html#timezone
-TIMEZONE = 'UTC'
-
-# Pelican will take the ``Date`` metadata and put the articles into folders
-# like ``/posts/2012/02/`` when generating the output.
-ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/'
-
-# I like to put everything into the category ``Blog``, which also appears on
-# the main menu. Tags will not appear on the menu.
-DEFAULT_CATEGORY = 'Blog'
-
-# I like to have ``Archives`` in the main menu.
-MENUITEMS = (
-    ('Archives', '{0}/archives.html'.format(SITEURL)),
-)
-
-
+# ===============
+# Basic settings
+# ===============
 AUTHOR = 'Seyna'
+DEFAULT_CATEGORY = 'blog'
 SITENAME = 'seyna.github.com'
-SITEURL = 'http://www.seyna.github.com'
+SITEURL = 'http://seyna.github.com'
+SITE_URL = SITEURL
+STATIC_PATHS = ['images', ]
+TIMEZONE = 'Asia/Taipei'
 
+
+# =============
+# URL settings
+# =============
+PERMALINK_STRUCTURE = '{date:%Y}/{date:%m}/'
+ARTICLE_URL = '%s{slug}.html' % PERMALINK_STRUCTURE
+ARTICLE_LANG_URL = '%s{slug}-{lang}.html' % PERMALINK_STRUCTURE
+PAGE_URL = '%spages/{slug}.html' % PERMALINK_STRUCTURE
+PAGE_LANG_URL = '%spages/{slug}-{lang}.html' % PERMALINK_STRUCTURE
+ARTICLE_SAVE_AS = '%s{slug}.html' % PERMALINK_STRUCTURE
+ARTICLE_LANG_SAVE_AS = '%s{slug}-{lang}.html' % PERMALINK_STRUCTURE
+PAGE_SAVE_AS = '%spages/{slug}.html' % PERMALINK_STRUCTURE
+PAGE_LANG_SAVE_AS = '%spages/{slug}-{lang}.html' % PERMALINK_STRUCTURE
+
+
+# ===========
+# Pagination
+# ===========
 WITH_PAGINATION = True
 DEFAULT_PAGINATION = 10
+
+
+# =================
+# Ordering content
+# =================
 REVERSE_ARCHIVE_ORDER = True
 
-# Uncomment what ever you want to use
-#GOOGLE_ANALYTICS = 'XX-XXXXXXX-XX'
-#DISQUS_SITENAME = 'yourdisqushandle'
-#GITHUB_URL = 'http://github.com/username/username.github.com'
-#TWITTER_USERNAME = 'username'
+
+# =================
+# Theming
+# =================
+THEME = 'notmyidea'
+
+DISQUS_SITENAME = 'seynacom'
+FLATTR = True
+GITHUB_URL = 'http://github.com/seyna/seyna.github.com'
+MENUITEMS = (
+    ('Archives', '{0}/archives.html'.format(SITEURL)),
+    )
+TWITTER_USERNAME = 'closetu'
